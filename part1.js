870d3414d042441eee193b0ddf9243a3866ef297
moskowitza <moskowitzaaron@mac.com>
2018-08-31 11:24:48 -0400

    fizzbuzz example of short circuit

--- part1.js
+++ part1.js
@@ -11,11 +11,35 @@
 
 // Biary Operators
 // > or < take two arguments, thus: Binary
+// let weather="sunny";
+// switch (weather) {
+//     case "rainy":
+//       console.log("Remember to bring an umbrella.");
+//       break;
+//     case "sunny":
+//       console.log("Dress lightly.");
+//     case "cloudy":
+//       console.log("Go outside.");
+//       break;
+//     default:
+//       console.log("Unknown weather type!");
+//       break;
+//   }
 
-let result = 1;
-let counter = 0;
-while (counter < 10) {
-  result = result * 2;
-  counter = counter + 1;
+  //Write a loop that makes seven calls to console.log to output the following triangle:
+  let row="*";
+  for (i=0;i<7;i++){
+      console.log(row);
+      row=row+"*";
+  }
+  /*Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+(This is actually an interview question that has been claimed to weed out a significant percentage of programmer candidates. So if you solved it, your labor market value just went up.)
+
+*/
+for (i=1;i<101;i++){
+    if(i%3 ==0 && i%5 ==0) {console.log("FizzBuzz"); }
+    else if(i%3 ==0){console.log("Fizz"); }
+    else if(i%5 ==0){console.log("Buzz"); }
+    else {console.log(i); }
 }
-console.log(result);
