a37243d9229f9057477c8c866edd0cf19d81e114
moskowitza <moskowitzaaron@mac.com>
2018-08-31 07:55:57 -0400

    back in

---
+++ part1.js
@@ -0,0 +1,13 @@
+// When you write something inside ${} in a 
+// template literal!
+`half of 100 is ${100 / 2}`
+
+// Unary operators
+//Not all operators are symbols. Some are written as words. 
+console.log(typeof 4.5)
+// → number
+console.log(typeof "x")
+// → string
+
+// Biary Operators
+// > or < take two arguments, thus: Binary
